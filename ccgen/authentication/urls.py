838e01d4dd1828e5ceb2a57c0b4923bab3769247
Victoria <adedayovicky123@gmail.com>
2022-08-07 14:11:55 +0100

    user authentication, login and register

--- ccgen/authentication/urls.py
+++ ccgen/authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register')
+    path('login', views.signin, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.signout, name="logout")
 ]
--- ccgen/ccgen/settings.py
+++ ccgen/ccgen/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ccgen',
     'homepage.apps.HomepageConfig',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
